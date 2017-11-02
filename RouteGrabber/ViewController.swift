54be6751d4a586143d017bf1c6c9b428ec34e4d5
Ben Lachman <blachman@mac.com>
2017-11-02 02:17:53 -0400

    Initial Commit

---
+++ RouteGrabber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RouteGrabber
+//
+//  Created by Ben Lachman on 11/2/17.
+//  Copyright © 2017 Nice Mohawk Limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
