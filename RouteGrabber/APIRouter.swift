78dc88555c6818b0a2e9461a25373982df7e37ae
Ben Lachman <blachman@mac.com>
2018-09-09 15:38:46 -0400

    Updating to send data to custom flask backend

--- RouteGrabber/APIRouter.swift
+++ RouteGrabber/APIRouter.swift
@@ -11,8 +11,9 @@ import Alamofire
 
 enum Constants {
     struct API {
-        static let serverPrefix = "https://api.jsonbin.io/"
-        static let secretKey = "$2a$10$BK8VRQ8.uRRAyPOnfvt3FuomRHiWcOYLN/JkbII73.hY7HzWTOtHW"
+//        static let serverPrefix = "https://match-tracks.service.nicemohawk.com"
+        static let serverPrefix = "http://10.0.1.10:8080"
+        static let secretKey = "hi-bob"
         // TODO: keep secret keys out of version control
     }
 }
@@ -31,7 +32,7 @@ extension Requestable {
         var mutableRequest = URLRequest(url: URL(string: URLString)!)
         mutableRequest.httpMethod = method.rawValue
 
-        mutableRequest.setValue(Constants.API.secretKey, forHTTPHeaderField: "secret-key")
+        mutableRequest.setValue("APIKey " + Constants.API.secretKey, forHTTPHeaderField: "Authorization")
         mutableRequest.setValue(String(true), forHTTPHeaderField: "private")
         mutableRequest.setValue("application/json", forHTTPHeaderField: "content-type")
 
@@ -44,6 +45,7 @@ extension Requestable {
 }
 
 struct Router {
+
     enum Workout: Requestable {
         case post(workouts: [MappableWorkout])
 
@@ -51,95 +53,27 @@ struct Router {
          return .post
         }
 
-        var path: String {
-            return "b"
+        var port: Int {
+            return 8080
         }
+
+        var path: String {
+            return "/devices/3cb65576-6bfd-4abb-ad23-168b7efda562/sessions"
+        }
+        
         var parameters: [String : Any]? {
             switch self {
             case .post(let workouts):
+                var params = [String : Any]()
 
-                // TODO: send up data from HKWorkout that isn't in the CLLocation data
-
-                // debug
-                // just send up the first workout with its first location coordinate
-
-
-                if workouts.count > 0 {
-                    let workout = workouts[0]
-                    if workout.locations.count > 0 {
-                        var locationsArray: [[String : Any]] = [[String : Any]]()
-                        for location in workout.locations {
-                            var coordinateDict = [String : Any]()
-                            coordinateDict["latitude"] = location.coordinate.latitude
-                            coordinateDict["longitude"] = location.coordinate.longitude
-
-                            var locationDict = [String : Any]()
-                            locationDict["coordinate"] = coordinateDict
-
-                            locationsArray.append(locationDict)
-                        }
-
-//                        let location = workout.locations[0]
-//
-//                        var coordinateDict = [String : Any]()
-//                        coordinateDict["latitude"] = location.coordinate.latitude
-//                        coordinateDict["longitude"] = location.coordinate.longitude
-//                        return coordinateDict
-//                        locationsArray.append(coordinateDict)
-
-                        print("Number of locations: \(locationsArray.count)")
-                        // 1299 > x > 1300
-                        let smallerLocationsArray: [[String : Any]] = Array(locationsArray[0..<1301])
-
-                        print("Sending up locations: \(smallerLocationsArray) \n Done listing locations before sendoff")
-
-                        var params = [String : Any]()
-                        params["locations"] = smallerLocationsArray
-                        return params
-
-
-                    } else {
-                        print("No locations this time")
-                    }
-
-                } else {
+                guard workouts.count > 0 else {
                     print("No workouts this time")
+                    return nil
                 }
 
+                params["sessions"] = workouts.map { $0.dictionary }
 
-
-                var workoutsArray = [[String : Any]]()
-                for mappableWorkout in workouts {
-                    var locationsArray = [[String : Any]]()
-                    for location in mappableWorkout.locations {
-                        var coordinateDict = [String : Any]()
-                        coordinateDict["latitude"] = location.coordinate.latitude
-                        coordinateDict["longitude"] = location.coordinate.longitude
-
-                        var locationDict = [String : Any]()
-                        locationDict["coordinate"] = coordinateDict
-
-                        // TODO: Send up all CLLocation data, including the following.
-                        // These should work, commented out to simplify debugging
-
-//                        locationDict["altitude"] = location.altitude
-//                        locationDict["timestamp"] = String(describing: location.timestamp)
-//                        locationDict["speed"] = location.speed
-//                        locationDict["course"] = location.course
-
-                        locationsArray.append(locationDict)
-                    }
-
-                    var workoutDict = [String : Any]()
-                    workoutDict["locations"] = locationsArray
-                    workoutsArray.append(workoutDict)
-                }
-
-                var parameters = [String : Any]()
-//                parameters["cllLocations"] = workoutsArray
-                parameters["data"] = "hello world"
-
-                return parameters
+                return params
             }
         }
 
--- RouteGrabber/MappableWorkout.swift
+++ RouteGrabber/MappableWorkout.swift
@@ -5,7 +5,6 @@
 //  Created by Robert Cantoni on 11/10/17.
 //  Copyright © 2017 Nice Mohawk Limited. All rights reserved.
 //
-
 import Foundation
 import HealthKit
 import CoreLocation
@@ -13,4 +12,36 @@ import CoreLocation
 struct MappableWorkout {
     var workout: HKWorkout
     var locations: [CLLocation]
+
+    var dictionary: [String: Any]? {
+        get {
+            // TODO: send up data from HKWorkout that isn't in the CLLocation data
+
+            if locations.count > 0 {
+                var dict = [String:Any]()
+                var coorinatesArray: [[Double]] = [[Double]]()
+                for location in locations {
+                    let coordinatePair = [location.coordinate.latitude, location.coordinate.longitude]
+                    
+                    coorinatesArray.append(coordinatePair)
+                }
+                
+//                if coorinatesArray.count > 10 {
+//                    let smallArray: [[Double]] = Array(coorinatesArray[0..<10])
+//
+//                    coorinatesArray = smallArray
+//                }
+
+                dict["track"] = ["coordinates": coorinatesArray]
+                let formatter = ISO8601DateFormatter()
+                dict["recorded_at"] = formatter.string(from: workout.startDate)
+
+                return dict
+            }
+            
+            print("No locations for this workout")
+
+            return nil
+        }
+    }
 }
